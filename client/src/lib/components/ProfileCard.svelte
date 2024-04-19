21643c8aec93fc495932029110ce607dd575ca16
yegor smertenko <ye.smertenko@gmail.com>
2024-04-19 11:01:12 -0700

    feat: update profile card text and styling in warhazard

--- client/src/lib/components/ProfileCard.svelte
+++ client/src/lib/components/ProfileCard.svelte
@@ -9,15 +9,15 @@
 		</div>
 		<div class="pt-20">
 			<p>Today</p>
-			<p class="text-plain-400 pt-1 font-normal">
+			<p class="pt-1 font-normal text-plain-400">
 				Despite the downturn in the tech market, I feel now is the perfect time for growth,
 				exploration, and software development. I spend my days learning and developing in Go,
 				exploring new research in deep learning, and working on open-source projects. I grew to
 				appreciate programming outside of University and found a great way to pass time. My
-				expertise lies in 'Data-driven development,' focusing on data analysis, visualization, and
-				backend development.
+				expertise lies in 'Data-driven development,' focusing on data engineering and backend
+				development.
 			</p>
-			<p class="font-reader text-plain-400 pt-4 italic">
+			<p class="pt-4 font-reader italic text-plain-400">
 				Note: A recent favorite discovery is the 'Go Time' podcast, which I highly recommend for
 				anyone curious about Go and programming in general.
 			</p>
