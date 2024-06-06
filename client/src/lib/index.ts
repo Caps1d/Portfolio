ed24f5a322330de4568f838096b8581c5c7a9af0
yegor smertenko <ye.smertenko@gmail.com>
2024-06-06 10:24:03 -0700

    feat: update project name

--- client/src/lib/index.ts
+++ client/src/lib/index.ts
@@ -1,47 +1,47 @@
 // place files you want to import through the `$lib` alias in this folder.
 
 type Project = {
-  id: string;
-  type: string;
-  name: string;
-  description: string;
-  status: Status;
-  year: string;
-  link: string;
-  hasPage: bool;
+	id: string;
+	type: string;
+	name: string;
+	description: string;
+	status: Status;
+	year: string;
+	link: string;
+	hasPage: bool;
 };
 
 export type Status = 'completed' | 'in-progress' | 'archived';
 
 export let projects: Project[] = [
-  {
-    id: 'proj-1',
-    type: 'data',
-    name: 'WarHazard',
-    description: 'Armed conflict data analysis & visualization',
-    status: 'completed',
-    year: '2023',
-    link: 'https://www.warhazard-ua.com/',
-    hasPage: true
-  },
-  {
-    id: 'proj-2',
-    type: 'web',
-    name: 'SnippetBox',
-    description: 'Blog-like web app written in Go',
-    status: 'completed',
-    year: '2023',
-    link: 'https://github.com/Caps1d/Lets-Go/tree/main',
-    hasPage: false
-  },
-  {
-    id: 'proj-3',
-    type: 'web',
-    name: 'GoShip',
-    description: 'Deployment tool written in Go',
-    status: 'in-progress',
-    year: '2024',
-    link: '',
-    hasPage: false
-  }
+	{
+		id: 'proj-1',
+		type: 'data',
+		name: 'WarHazard',
+		description: 'Armed conflict data analysis & visualization',
+		status: 'completed',
+		year: '2023',
+		link: 'https://www.warhazard-ua.com/',
+		hasPage: true
+	},
+	{
+		id: 'proj-2',
+		type: 'web',
+		name: 'SnippetWall',
+		description: 'Blog-like web app written in Go',
+		status: 'completed',
+		year: '2023',
+		link: 'https://github.com/Caps1d/Lets-Go/tree/main',
+		hasPage: false
+	},
+	{
+		id: 'proj-3',
+		type: 'web',
+		name: 'GoShip',
+		description: 'Deployment tool written in Go',
+		status: 'in-progress',
+		year: '2024',
+		link: '',
+		hasPage: false
+	}
 ];
