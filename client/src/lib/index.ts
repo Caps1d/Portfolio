2de0d1f7c68cb20fd69c6f4efcdf21ff87a1efc7
yegor smertenko <ye.smertenko@gmail.com>
2024-06-12 10:18:21 -0700

    feat: add project-4

--- client/src/lib/index.ts
+++ client/src/lib/index.ts
@@ -1,47 +1,57 @@
 // place files you want to import through the `$lib` alias in this folder.
 
 type Project = {
-	id: string;
-	type: string;
-	name: string;
-	description: string;
-	status: Status;
-	year: string;
-	link: string;
-	hasPage: bool;
+  id: string;
+  type: string;
+  name: string;
+  description: string;
+  status: Status;
+  year: string;
+  link: string;
+  hasPage: boolean;
 };
 
 export type Status = 'completed' | 'in-progress' | 'archived';
 
 export let projects: Project[] = [
-	{
-		id: 'proj-1',
-		type: 'data',
-		name: 'WarHazard',
-		description: 'Armed conflict data analysis & visualization',
-		status: 'completed',
-		year: '2023',
-		link: 'https://www.warhazard-ua.com/',
-		hasPage: true
-	},
-	{
-		id: 'proj-2',
-		type: 'web',
-		name: 'SnippetWall',
-		description: 'Blog-like web app written in Go',
-		status: 'completed',
-		year: '2023',
-		link: 'https://github.com/Caps1d/Lets-Go/tree/main',
-		hasPage: false
-	},
-	{
-		id: 'proj-3',
-		type: 'web',
-		name: 'GoShip',
-		description: 'Deployment tool written in Go',
-		status: 'in-progress',
-		year: '2024',
-		link: '',
-		hasPage: false
-	}
+  {
+    id: 'proj-1',
+    type: 'data',
+    name: 'WarHazard',
+    description: 'Armed conflict data analysis & visualization',
+    status: 'completed',
+    year: '2023',
+    link: 'https://www.warhazard-ua.com/',
+    hasPage: true
+  },
+  {
+    id: 'proj-2',
+    type: 'web',
+    name: 'SnippetWall',
+    description: 'Blog-like web app written in Go',
+    status: 'completed',
+    year: '2023',
+    link: 'https://github.com/Caps1d/Lets-Go/tree/main',
+    hasPage: false
+  },
+  {
+    id: 'proj-3',
+    type: 'web',
+    name: 'GoShip',
+    description: 'Deployment tool written in Go',
+    status: 'in-progress',
+    year: '2024',
+    link: '',
+    hasPage: false
+  },
+  {
+    id: 'proj-4',
+    type: 'web',
+    name: 'Babyn Yar Memorial Center',
+    description: 'Website and CMS for the Babyn-Yar memorial',
+    status: 'completed',
+    year: '2023',
+    link: 'https://babynyar.org.ua/',
+    hasPage: false
+  }
 ];
