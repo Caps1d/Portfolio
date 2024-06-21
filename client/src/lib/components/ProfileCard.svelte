e0e238f8e76377304ff230ac3be647edc2acecb0
yegor smertenko <ye.smertenko@gmail.com>
2024-06-21 14:51:36 -0700

    feat(ProfileCard): updated profile text

--- client/src/lib/components/ProfileCard.svelte
+++ client/src/lib/components/ProfileCard.svelte
@@ -5,17 +5,22 @@
 			<p>Yegor Smertenko</p>
 		</div>
 		<div class="pt-1">
-			<p class="text-plain-400">Software Engineer | Data Analyst</p>
+			<p class="text-plain-400">Software Engineer | Cloud Solutions</p>
 		</div>
 		<div class="pt-20">
 			<p>Today</p>
 			<p class="pt-1 font-normal text-plain-400">
 				Despite the downturn in the tech market, I feel now is the perfect time for growth,
-				exploration, and software development. I spend my days learning and developing in Go,
-				exploring new research in deep learning, and working on open-source projects. I grew to
-				appreciate programming outside of University and found a great way to pass time. My
-				expertise lies in 'Data-driven development,' focusing on data engineering and backend
-				development.
+				exploration, and software development. Us, people in tech, are caught in turbulent times and
+				now, more than ever its important that we keep up with our tools and industry as a whole. As
+				a software engineer my passion lies in cloud-native development and large scale software
+				architecture. I describe myself as a language generalist but most of my experience is with
+				Go, JavaScript, TypeScript, Java, and Python. Outside of software development you can likely
+				catch me in the gym or playing volleyball with friends on the weekend.
+			</p>
+			<p>
+				P.S: If you're into bodybuilding feel free to reach out as I'm looking for serious gym
+				friends here in Vancouver.
 			</p>
 			<p class="pt-4 font-reader italic text-plain-400">
 				Note: A recent favorite discovery is the 'Go Time' podcast, which I highly recommend for
